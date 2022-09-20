3b43bb77a140e5f1a0c10ae918e20f32956dc963
Sanjeeth <111098807+Sanjeeth8002@users.noreply.github.com>
2022-09-20 15:40:49 +0530

    Update Program.java

--- Ex - 5/Ex - 5.1/Program.java
+++ Ex - 5/Ex - 5.1/Program.java
@@ -1,1 +1,20 @@
+package Employee_data;
 
+import Employee_data.pack1.Employee;
+
+public class file {
+    public static void main(String args[]) {
+        Employee emp[] = new Employee[5];
+
+        for (int x = 0; x < 5; x++) {
+            System.out.println("\n\nEntering Details of Employee " + (x + 1) + "\n\n");
+            emp[x] = new Employee();
+        }
+        for (int x = 0; x < 5; x++) {
+            System.out.println("\n\nDisplaying Details of Employee " + (x + 1) + "\n\n");
+            emp[x].do_pay();
+            emp[x].calc_taxes();
+            emp[x].generate_slip();
+        }
+    }
+}
