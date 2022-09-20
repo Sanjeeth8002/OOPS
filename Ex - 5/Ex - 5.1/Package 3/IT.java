290c153cc614b98e3dcce50f0475ad75b2db5e3d
Sanjeeth <111098807+Sanjeeth8002@users.noreply.github.com>
2022-09-20 15:40:12 +0530

    Update IT.java

--- Ex - 5/Ex - 5.1/Package 3/IT.java
+++ Ex - 5/Ex - 5.1/Package 3/IT.java
@@ -1,1 +1,32 @@
+package Employee_data.pack3;
 
+public class IncomeTax {
+    protected int res = 0;
+    protected String category = "";
+
+    protected IncomeTax() {
+    }
+
+    protected void do_tax(int Basic_pay) {
+        if (category.contains("male") || category.contains("Male")) {
+            if (Basic_pay <= 190000)
+                res = 0;
+            else if (Basic_pay <= 200000)
+                res = ((Basic_pay) / 100) * 10;
+            else if (Basic_pay <= 500000)
+                res = (Basic_pay / 100) * 20;
+            else if (Basic_pay > 500000)
+                res = (Basic_pay / 100) * 25;
+        }
+        if (category.contains("female") || category.contains("Female")) {
+            if (Basic_pay <= 190000)
+                res = 0;
+            else if (Basic_pay <= 200000)
+                res = 0;
+            else if (Basic_pay <= 500000)
+                res = (Basic_pay / 100) * 10;
+            else if (Basic_pay > 500000)
+                res = (Basic_pay / 100) * 20;
+        }
+    }
+}
